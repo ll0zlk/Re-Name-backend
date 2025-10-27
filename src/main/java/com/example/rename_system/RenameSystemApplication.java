b8fddd425fa34e5c8a6554c9f15a9ae9fc6b8652
ll0zlk <hyojin041025@gmail.com>
2025-10-27 16:30:50 +0900

    Create project

---
+++ src/main/java/com/example/rename_system/RenameSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.rename_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenameSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenameSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rename_system/RenameSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rename_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenameSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
